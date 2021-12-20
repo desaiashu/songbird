022759dc8db76b30e4d454be1457de276ac1f503
Ashu Desai <ashuddesai@gmail.com>
2021-12-19 18:12:36 -0800

    tweaks

--- code.py
+++ code.py
@@ -4,10 +4,12 @@ import songbird.clock.clock as clock
 
 from MIDIFile import MIDIFile
 
-m = MIDIFile('midifiles/rats.mid')
+m = MIDIFile('midifiles/Grooves From Mars/CR-78/Patterns/CR-78 Waltz A.mid')
 m.parse()
-print(m.tracks)
-print(m.tracks[0])
+for idx, track in enumerate(m):
+    track.parse()
+    print(f'Track {idx}:')
+    print(str(track))
 print('yay')
 
 # Run loop
--- lib/MIDIFile/util.py
+++ lib/MIDIFile/util.py
@@ -21,12 +21,9 @@ class SafeEnum(type):
 
     def __init__(self, code):
         self.code = code
-        print(code)
 
     def __str__(self):
-        # return str(self.value)
-        return str(self.__class__.__dict__.keys()[self.__class__.__dict__.values().index(self.code)])
-        # return self.name.replace('_',' ')
+        return str(list(self.__class__.__dict__.keys())[list(self.__class__.__dict__.values()).index(self.code)])
 
     @classmethod
     def make(cls,n):
@@ -41,12 +38,6 @@ class ConversionEnum(SafeEnum):
     def __init__(self,*args):
         self.code=args[0]
         self.converter=args[1] if len(args)>1 else Converter.Id1
-
-    # @classmethod
-    # def make(cls, n):
-    #     for obj in cls:
-    #         if obj.code==n: return obj
-    #     return None
 
     def __call__(self,value):
         return self.converter(value)
